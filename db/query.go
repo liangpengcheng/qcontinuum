d0afc6a3ece7aaab3a3cc36491d26cfb6a62db1a
liang <5459489@qq.com>
2017-03-15 22:36:10 +0800

    进度很慢

--- db/query.go
+++ db/query.go
@@ -4,8 +4,8 @@ import "github.com/golang/protobuf/proto"
 
 // IQuery 接口
 type IQuery interface {
-	Get(key string) string
-	Set(key string, v string)
-	GetObj(key string, obj proto.Message)
-	SetObj(key string, obj proto.Message)
+	Get(key string, ptrRet interface{})
+	Set(key string, v string, expiry uint32)
+	GetObj(key string, obj proto.Message, expiry uint32)
+	SetObj(key string, obj proto.Message, expiry uint32)
 }
--- db/rediscouchbase.go
+++ db/rediscouchbase.go
@@ -3,7 +3,8 @@ package db
 import "github.com/garyburd/redigo/redis"
 
 type rediscouchbaseQuery struct {
-	node *RedisNode
+	node      *RedisNode
+	couchNode *CouchbaseCluster
 }
 
 func (rc *rediscouchbaseQuery) Get(key string) string {
@@ -14,12 +15,19 @@ func (rc *rediscouchbaseQuery) Get(key string) string {
 		return ret
 	}
 	//从couchbase读取
-	return ""
+	var retstr string
+	rc.couchNode.bucket.Get(key, &retstr)
+	conn.Do("set", key, retstr)
+	return retstr
 }
 
-func (rc *rediscouchbaseQuery) Set(key string, v string) {
+func (rc *rediscouchbaseQuery) Set(key string, v string, expiry uint32) {
 	conn := rc.node.GetRedis()
 	defer rc.node.Put(conn)
 	conn.Do("set", key, v)
+	if expiry > 0 {
+		conn.Do("expire", key, expiry)
+	}
 	//设置couchbase
+	rc.couchNode.bucket.Upsert(key, v, expiry)
 }
