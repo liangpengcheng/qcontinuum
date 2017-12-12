dcd5b9286f7c0e35904e9f9ce6a24825b91703e7
liangpengcheng <5459489@qq.com>
2017-12-12 11:16:22 +0800

    uuid的格式处理

--- base/uuid.go
+++ base/uuid.go
@@ -20,10 +20,10 @@ var uuidRegex *regexp.Regexp = regexp.MustCompile(`^\{?([a-fA-F0-9]{8})-?([a-fA-
 // UUID type.
 type UUID [16]byte
 
-// Hex returns a hex string representation of the UUID in xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx format.
+// Hex returns a hex string representation of the UUID in xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx format.
 func (this UUID) Hex() string {
 	x := [16]byte(this)
-	return fmt.Sprintf("%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+	return fmt.Sprintf("%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
 		x[0], x[1], x[2], x[3], x[4],
 		x[5], x[6],
 		x[7], x[8],
