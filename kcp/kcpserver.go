edd35ca40a0552d7b9678fb38472f21004a2289f
liangpengcheng <5459489@qq.com>
2017-12-13 20:47:40 +0800

    将kcp的实现独立一个包，在不使用的情况下，就不会调用他的init函数。

---
+++ kcp/kcpserver.go
@@ -0,0 +1,83 @@
+package kcp
+
+import (
+	"github.com/liangpengcheng/qcontinuum/base"
+	"github.com/liangpengcheng/qcontinuum/network"
+	_kcp "github.com/xtaci/kcp-go"
+)
+
+// Server kcp server
+type Server struct {
+	Listener *_kcp.Listener
+}
+
+// NewKCPServer 创建一个kcp server
+func NewKCPServer(host string) (*Server, error) {
+	lis, err := _kcp.ListenWithOptions(host, nil, 0, 0)
+	if err != nil {
+		return nil, err
+	}
+	// 默认decp
+	if err := lis.SetDSCP(0); err != nil {
+		lis.Close()
+		return nil, err
+	}
+	if err := lis.SetReadBuffer(4194304); err != nil {
+		lis.Close()
+		return nil, err
+	}
+	if err := lis.SetWriteBuffer(4194304); err != nil {
+		lis.Close()
+		return nil, err
+	}
+	return &Server{
+		Listener: lis,
+	}, nil
+
+}
+
+// BlockAccept 阻塞收消息
+func (s *Server) BlockAccept(proc *network.Processor) {
+	for {
+		if conn, err := s.Listener.AcceptKCP(); err == nil {
+			base.LogInfo("remote address:%s", conn.RemoteAddr().String())
+			setupKcp(conn)
+			peer := &network.ClientPeer{
+				Connection:   conn,
+				RedirectProc: make(chan *network.Processor, 1),
+				Proc:         proc,
+			}
+
+			go peer.ConnectionHandler()
+		} else {
+			base.LogError("accept error :%s", err.Error())
+			break
+		}
+	}
+	base.LogInfo("exit accept")
+}
+
+// BlockAcceptOne 接受一个连接
+func (s *Server) BlockAcceptOne(proc *network.Processor) {
+	if conn, err := s.Listener.AcceptKCP(); err == nil {
+		base.LogInfo("remote address:%s", conn.RemoteAddr().String())
+		setupKcp(conn)
+		peer := &network.ClientPeer{
+			Connection:   conn,
+			RedirectProc: make(chan *network.Processor, 1),
+			Proc:         proc,
+		}
+
+		go peer.ConnectionHandler()
+	}
+}
+
+func setupKcp(conn *_kcp.UDPSession) {
+	conn.SetStreamMode(false)
+	conn.SetWriteDelay(false)
+	// 这个参数需要好好研究
+	conn.SetNoDelay(1, 10, 2, 1)
+	conn.SetMtu(1400)
+	conn.SetWindowSize(4096, 4096)
+	conn.SetACKNoDelay(true)
+}
