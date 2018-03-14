a800a7d6448d866c2874b75df19d335bd7353690
liangpengcheng <5459489@qq.com>
2018-03-14 14:05:04 +0800

    时间相关

--- base/time.go
+++ base/time.go
@@ -18,3 +18,10 @@ func UntilNextWeek() int {
 	leftsec := (7-int(w))*24*60*60 - now.Hour()*60*60 - (now.Minute()+5)*60 - now.Second()
 	return leftsec
 }
+
+// DeltaDays 两个时间相差几天 ，都是unix秒
+func DeltaDays(day1Unix, day2Unix int64) int {
+	d1 := time.Unix(day1Unix, 0)
+	d2 := time.Unix(day2Unix, 0)
+	return (d2.Year()-d1.Year())*365 + d2.YearDay() - d1.YearDay()
+}
