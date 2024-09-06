e2195ec917e985c2f4857a0130a742c6d1988fc8
梁鹏程 <liangpengcheng@users.noreply.github.com>
2024-09-06 21:49:32 +0800

    Update zap.go

--- base/zap.go
+++ base/zap.go
@@ -1,7 +1,11 @@
 package base
 
 import (
+	"flag"
+	"os"
+
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var zp *zap.Logger
@@ -13,30 +17,29 @@ func init() {
 
 	logPath := flag.String("logpath", "", "log file path")
 	flag.Parse()
-	if logPath!=""{
+	if logPath != "" {
 		// 创建文件写入器
 		file, _ := os.Create(*logPath)
 		writeSyncer := zapcore.AddSync(file)
-	
+
 		// 配置日志编码器
 		encoderConfig := zap.NewProductionEncoderConfig()
 		encoderConfig.TimeKey = "time"
 		encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	
+
 		// 设置日志级别和输出目标
 		core := zapcore.NewCore(
 			zapcore.NewJSONEncoder(encoderConfig), // 使用 JSON 编码器
-			writeSyncer,                            // 输出到指定文件
-			zap.InfoLevel,                          // 日志级别
+			writeSyncer,                           // 输出到指定文件
+			zap.InfoLevel,                         // 日志级别
 		)
-	
+
 		zp = zap.New(core)
 		defer zp.Sync()
-	}else{
+	} else {
 		zp, _ = zap.NewProduction()
 	}
-	
-	
+
 	//
-	
+
 }
