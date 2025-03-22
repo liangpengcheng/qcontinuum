f2eb0984f6ef4e1e53cb3090b31166ae74e87940
lpc <545@qq.com>
2025-03-22 17:47:59 +0800

    websocket报错处理

--- network/peer.go
+++ network/peer.go
@@ -192,7 +192,7 @@ func (peer *ClientPeer) ConnectionHandler() {
 }
 
 // ConnectionHandler read messages here
-func (peer *ClientPeer) ConnectionHandlerWithPreFunc(f func()) {
+func (peer *ClientPeer) ConnectionHandlerWithPreFunc(f func() bool) {
 	defer func() {
 		if r := recover(); r != nil {
 			if _, ok := r.(net.Error); ok {
@@ -208,7 +208,9 @@ func (peer *ClientPeer) ConnectionHandlerWithPreFunc(f func()) {
 	}
 
 	for {
-		f()
+		if !f() {
+			break
+		}
 		h, buffer, err := ReadMessage(peer.Connection)
 		if len(buffer) == 0 {
 			continue
