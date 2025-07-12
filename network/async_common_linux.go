6e56d7414e3505be93626e86201805280e2f8a5a
梁鹏程 <liangpengcheng@users.noreply.github.com>
2025-07-12 13:34:29 +0800

    fix overflow error

--- network/async_common_linux.go
+++ network/async_common_linux.go
@@ -6,7 +6,7 @@ import "syscall"
 
 // Linux平台的事件常量
 const (
-	EpollIn  = uint32(syscall.EPOLLIN)
-	EpollOut = uint32(syscall.EPOLLOUT)
-	EpollET  = uint32(syscall.EPOLLET)
+	EpollIn  = uint32(syscall.EPOLLIN)   // 0x001
+	EpollOut = uint32(syscall.EPOLLOUT)  // 0x004
+	EpollET  = uint32(0x80000000)        // EPOLLET = 0x80000000
 )
